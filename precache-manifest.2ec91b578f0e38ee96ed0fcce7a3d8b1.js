be954dd03d30f6ab585f8cbe095bfeabae1e44ca
Alexandr Moroz <afmoroz@gmail.com>
2020-09-12 18:11:51 +0300

    Updates

---
+++ precache-manifest.2ec91b578f0e38ee96ed0fcce7a3d8b1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81ab55b25b7fec6adb8e98041b1cd515",
+    "url": "/react-minimal-site-two/index.html"
+  },
+  {
+    "revision": "d795adc69aff19fd09dc",
+    "url": "/react-minimal-site-two/static/css/main.af96e90b.chunk.css"
+  },
+  {
+    "revision": "40bca51c1bcae5a6d081",
+    "url": "/react-minimal-site-two/static/js/2.c64bdb59.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-minimal-site-two/static/js/2.c64bdb59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d795adc69aff19fd09dc",
+    "url": "/react-minimal-site-two/static/js/main.1d198f40.chunk.js"
+  },
+  {
+    "revision": "36b6f3322764c5459e50",
+    "url": "/react-minimal-site-two/static/js/runtime-main.a7e75afb.js"
+  },
+  {
+    "revision": "3102be6efa1140d66018a351b81845d6",
+    "url": "/react-minimal-site-two/static/media/Berghain.3102be6e.jpg"
+  },
+  {
+    "revision": "2b0cfd7ea12dfa7b238aa6f0786b860d",
+    "url": "/react-minimal-site-two/static/media/Berlin.2b0cfd7e.jpg"
+  },
+  {
+    "revision": "04f34c74c7f36f673fc457276014c8b3",
+    "url": "/react-minimal-site-two/static/media/Glastonbury.04f34c74.webp"
+  },
+  {
+    "revision": "91f5c2541e0010c3ca8b656b17b7623d",
+    "url": "/react-minimal-site-two/static/media/HarpersBazaar.91f5c254.jpg"
+  },
+  {
+    "revision": "80deea0efd820901083de4b13eae8870",
+    "url": "/react-minimal-site-two/static/media/London.80deea0e.jpg"
+  },
+  {
+    "revision": "c522cbdf5d5c8007c3a33507e345cfba",
+    "url": "/react-minimal-site-two/static/media/Malmo.c522cbdf.jpg"
+  },
+  {
+    "revision": "46df4ee0d333e419e10ee70f93871de9",
+    "url": "/react-minimal-site-two/static/media/birds.46df4ee0.jpg"
+  }
+]);
